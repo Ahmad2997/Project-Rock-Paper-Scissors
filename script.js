c926d4df7e81fdeea4a194c40201aac52db6b478
Ahmad2997 <126724352+Ahmad2997@users.noreply.github.com>
2025-12-24 01:13:24 +0100

    define getHumanChoice and add logic

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //console.log("hello, world!");
 let ComputerScore=0;
 let HumanScore=0;
+console.log(getHumanChoice());
+console.log(getComputerChoice());
 function getComputerChoice(){
     return  getRandomInt(3);
 }
-console.log(getComputerChoice());
 function getHumanChoice(){
-
+ let humanChoice=prompt("Rock, Paper, Scissor?");
+ return humanChoice;
 }
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
